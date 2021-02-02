5492fc1cfe91b37350874cd77c791b4670aa485a
Vadim Smirnov <smirnou.vadzim@gmail.com>
2021-02-03 00:27:12 +0300

    add isSubsequence

---
+++ coding-problems/leetcode/is-subsequence/is-subsequence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+let isSubsequence = function(s, t) {
+    let i = 0;
+    let j = 0;
+
+    while(i < s.length) {
+        if(j === t.length) {
+            return false;
+        }
+        if(s[i] === t[j]) {
+            i++;
+        }
+        j++;
+    }
+return true;
+};
