a1c82321aa7daa1361854d1ffb41f4a73f971d98
Vadim Smirnov <smirnou.vadzim@gmail.com>
2021-02-03 00:07:46 +0300

    add fizzbuzz

---
+++ coding-problems/random/fizzbuzz/fizzbuzz-second.js
@@ -0,0 +1,10 @@
+function fizzBuzz(num) {
+    if (num <= 0) return null;
+
+    for(let i = 1; i <= num; i++) {
+        let str = "";
+        if(!(i % 3)) str += "Fizz";
+        if(!(i % 5)) str += "Buzz";
+        console.log(str || i);
+    }
+}
---
+++ coding-problems/random/fizzbuzz/fizzbuzz-ternary.js
@@ -0,0 +1,8 @@
+function fizzBuzz(num) {
+    if (num <= 0) return null;
+
+    for (var i = 1; i <= num; i++) {
+        var fizz = i % 3 == 0, buzz = i % 5 == 0;
+        console.log(fizz ? buzz ? "FizzBuzz" : "Fizz" : buzz ? "Buzz" : i);
+    }
+}
